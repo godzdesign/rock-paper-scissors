3c193d4a687eb72b6eee8a72da84cbd671434bf1
godzdesign <godzdesign@users.noreply.github.com>
2025-07-29 14:28:04 +0200

    This is will be a better version of my rock paper scissors game and also will be a application!

---
+++ renderer.js
@@ -0,0 +1,13 @@
+
+//starting gameMenu
+function startGame() {
+    window.location.href = "game.html";
+}
+
+// echo "# rock-paper-scissors" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/godzdesign/rock-paper-scissors.git
+// git push -u origin main
